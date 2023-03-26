7c86947a301c0c981f2f2ebfa2fd3068c828028e
jyjdal <1604070358@qq.com>
2023-03-26 15:25:58 +0800

    Add test report.

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,14 @@
 import unittest
 
+from BeautifulReport import BeautifulReport
+
+from common import base_path
+
 
 def main():
     suite = unittest.defaultTestLoader.discover(start_dir=".", pattern="test_*.py")
-    runner = unittest.TextTestRunner()
-    runner.run(suite)
+    result = BeautifulReport(suite)
+    result.report(filename="test_report.html", description="测试报告", report_dir=base_path / "outputs")
 
 
 if __name__ == '__main__':
