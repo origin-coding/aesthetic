f5e3590b87133aeb5cbd3834ded5a8509ba0b588
jyjdal <1604070358@qq.com>
2023-03-30 19:04:04 +0800

    Delete unused variable.

--- common/__init__.py
+++ common/__init__.py
@@ -2,5 +2,3 @@ from .common import TrainData, TensorData, image_transforms, base_path
 from .config import setup_config
 
 __all__ = ["TrainData", "TensorData", "image_transforms", "base_path", "setup_config"]
-
-batch_size = 100
