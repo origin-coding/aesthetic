1bc9c106bf3f55cd5e75c81e0eddc802a549d044
jyjdal <1604070358@qq.com>
2023-04-06 21:43:27 +0800

    Add loss function.

--- models/__init__.py
+++ models/__init__.py
@@ -1,4 +1,9 @@
 from .cbam import ChannelAttention, SpacialAttention, CBAM
+from .loss import MTLoss
 from .model import MTAesthetic, SharedLayer
 
-__all__ = ["ChannelAttention", "SpacialAttention", "CBAM", "MTAesthetic", "SharedLayer"]
+__all__ = [
+    "ChannelAttention", "SpacialAttention", "CBAM",  # CBAM及Attention相关模型
+    "MTAesthetic", "SharedLayer",  # MTAesthetic基础模型
+    "MTLoss"  # 自定义损失函数
+]
---
+++ models/loss.py
@@ -0,0 +1,21 @@
+from torch import Tensor
+from torch.nn import Module, BCELoss, MultiLabelMarginLoss, SmoothL1Loss
+
+from common import TensorData
+
+
+class MTLoss(Module):
+
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        self.loss_fn_bin = BCELoss()
+        self.loss_fn_score = SmoothL1Loss()
+        self.loss_fn_attribute = MultiLabelMarginLoss()
+
+    def forward(self, input_tensor: TensorData, label_tensor: TensorData) -> Tensor:
+        loss_bin: Tensor = self.loss_fn_bin(input_tensor.binary, label_tensor.binary)
+        loss_score: Tensor = self.loss_fn_score(input_tensor.score, label_tensor.score)
+        loss_attribute: Tensor = self.loss_fn_attribute(input_tensor.attribute, label_tensor.attribute)
+
+        # 这里将三个子任务的损失值相加
+        return loss_bin + loss_score + loss_attribute
