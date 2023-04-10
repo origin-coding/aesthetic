231d1bf864d0801e0de4cfc2226cf8e81e6d9566
jyjdal <1604070358@qq.com>
2023-04-10 20:05:18 +0800

    Add tests.

--- tests/test_model.py
+++ tests/test_model.py
@@ -2,7 +2,8 @@ import unittest
 
 import torch
 
-from models import ChannelAttention, SpacialAttention, CBAM, SharedLayer
+from common import TensorData
+from models import ChannelAttention, SpacialAttention, CBAM, MTAesthetic
 
 
 class TestModelOutputShape(unittest.TestCase):
@@ -36,12 +37,14 @@ class TestModelOutputShape(unittest.TestCase):
             torch.Size((5, 32, 256, 256))
         )
 
-    def test_shared_layer_shape(self):
-        shared_layer = SharedLayer()
-        input_tensor = torch.rand(100, 3, 256, 256)
-        output_tensor = shared_layer(input_tensor)
-
-        self.assertEqual(
-            output_tensor.shape,
-            torch.Size((100, 1024, 16, 16))
+    def test_model(self):
+        model = MTAesthetic(channels=512, kernel_size=3)
+        input_tensor = TensorData(
+            binary=torch.rand(5, 3, 256, 256),
+            score=torch.rand(5, 3, 256, 256),
+            attribute=torch.rand(5, 3, 256, 256)
         )
+        output_tensor: TensorData = model(input_tensor)
+        print(output_tensor["binary"])
+        print(output_tensor["score"].shape)
+        print(output_tensor["attribute"].shape)
