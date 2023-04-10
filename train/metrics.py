a858ea510fbb28982e4d95a7b28444400c9eccb2
jyjdal <1604070358@qq.com>
2023-04-10 14:23:02 +0800

    Add metrics.

---
+++ train/metrics.py
@@ -0,0 +1,31 @@
+from typing import Dict, Tuple
+
+import torch
+from ignite.metrics import Metric, Loss, Accuracy, ClassificationReport, MeanSquaredError
+from torch import Tensor
+
+from common import TrainStepOutput
+from models import MTLoss
+
+
+def extract_binary(output: TrainStepOutput) -> Tuple[Tensor, Tensor]:
+    return output[0]["binary"], output[1]["binary"]
+
+
+def extract_score(output: TrainStepOutput) -> Tuple[Tensor, Tensor]:
+    return output[0]["score"], output[1]["score"]
+
+
+def extract_attribute(output: TrainStepOutput) -> Tuple[Tensor, Tensor]:
+    return output[0]["attribute"], output[1]["attribute"]
+
+
+def setup_metrics(device: torch.device) -> Dict[str, Metric]:
+    return {
+        "loss": Loss(loss_fn=MTLoss(), device=device),
+        "bin": Accuracy(output_transform=extract_binary, device=device),
+        "score": MeanSquaredError(output_transform=extract_score, device=device),
+        "attribute": ClassificationReport(
+            output_dict=True, is_multilabel=True, output_transform=extract_attribute, device=device
+        )
+    }
