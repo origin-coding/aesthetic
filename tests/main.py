9b4d2fe77a74393e6a06423ca54d7001227a94e0
jyjdal <1604070358@qq.com>
2023-04-18 11:37:07 +0800

    Change test output path to 'output_path'.

--- tests/main.py
+++ tests/main.py
@@ -2,10 +2,10 @@ import unittest
 
 from BeautifulReport import BeautifulReport
 
-from common import base_path
+from common import output_path
 
 
 def test_main(filename: str, description: str):
     suite = unittest.defaultTestLoader.discover(start_dir=".", pattern="test_*.py")
     result = BeautifulReport(suite)
-    result.report(filename=filename, description=description, report_dir=base_path / "outputs")
+    result.report(filename=filename, description=description, report_dir=output_path)
