e93f2f4477c2aa661028abd1a8007e43bf1b64b7
sun_qiang <1604070358@qq.com>
2023-05-19 16:41:18 +0800

    Add type definition for image assessment result.

--- common/__init__.py
+++ common/__init__.py
@@ -1,9 +1,10 @@
 from .common import base_path, data_path, output_path, checkpoint_path, log_path
 from .common import image_transforms
-from .types import TensorData, TrainData, StepOutput
+from .types import TensorData, TrainData, StepOutput, AttributeResult, AssessResult, new_attribute_result
 
 __all__ = [
     "image_transforms",
     "base_path", "data_path", "output_path", "checkpoint_path", "log_path",
-    "TensorData", "TrainData", "StepOutput"
+    "TensorData", "TrainData", "StepOutput",
+    "AttributeResult", "AssessResult", "new_attribute_result"
 ]
--- common/types.py
+++ common/types.py
@@ -1,8 +1,9 @@
-from typing import TypedDict, Tuple
+from typing import TypedDict, Tuple, List
 
 from torch import Tensor
 
 
+# 训练过程中用到的变量类型
 class TensorData(TypedDict):
     binary: Tensor
     attribute: Tensor
@@ -15,3 +16,44 @@ class TrainData(TypedDict):
 
 
 StepOutput = Tuple[TensorData, TensorData]
+
+
+# 应用程序用到的数据类型
+class AttributeResult(TypedDict):
+    """
+    图像美学多标签评价的标签名
+    """
+    balancing_element: bool  # 平衡
+    content: bool  # 内容
+    color_harmony: bool  # 颜色和谐
+    depth_of_field: bool  # 景深
+    lighting: bool  # 光照
+    motion_blur: bool  # 运动模糊
+    object_emphasis: bool  # 主体强调
+    rule_of_thirds: bool  # 三分法
+    vivid_color: bool  # 颜色鲜明
+    repetition: bool  # 重复
+    symmetry: bool  # 对称性
+
+
+def new_attribute_result(results: List[bool]) -> AttributeResult:
+    assert len(results) == 11  # 首先判断列表长度是否正确
+    return AttributeResult(
+        depth_of_field=results[0],
+        balancing_element=results[1],
+        color_harmony=results[2],
+        content=results[3],
+        lighting=results[4],
+        motion_blur=results[5],
+        object_emphasis=results[6],
+        repetition=results[7],
+        symmetry=results[8],
+        rule_of_thirds=results[9],
+        vivid_color=results[10]
+    )
+
+
+class AssessResult(TypedDict):
+    binary: bool
+    score: float
+    attribute: AttributeResult
